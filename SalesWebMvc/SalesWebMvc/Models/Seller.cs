06d9758d18cd297e1935c559942b249855f01253
Luisandro N. Bandeira <luisandro@hotmail.com>
2021-05-21 11:12:49 -0300

    SellerService and basic FindAll

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
@@ -9,6 +10,7 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]//Isso serve para formatar a data de nascimento
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department{ get; set; }
