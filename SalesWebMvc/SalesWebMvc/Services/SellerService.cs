641e10375b922173f61e708976f5aaf81ef85048
Luisandro N. Bandeira <luisandro@hotmail.com>
2021-05-21 15:10:51 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First(); //temporário só para forçar um id de departamento no vendedor
             _context.Add(obj);
             _context.SaveChanges();
         }
