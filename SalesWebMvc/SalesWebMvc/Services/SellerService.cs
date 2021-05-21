ff8a76bbfc11378fc16c9edcd144b1790a61a4b3
Luisandro N. Bandeira <luisandro@hotmail.com>
2021-05-21 15:37:10 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -23,7 +23,6 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First(); //temporário só para forçar um id de departamento no vendedor
             _context.Add(obj);
             _context.SaveChanges();
         }
